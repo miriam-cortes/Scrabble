141a66d021c86270abe9456bc56c79cbee302c75
Miriam Cortes <mcortes.umiami@gmail.com>
2016-08-31 13:11:54 -0700

    Player class added but not tested

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -2,8 +2,6 @@ require_relative '../Scrabble'
 require 'awesome_print'
 
 class Scrabble::Scoring
-  @@hash_of_scores = {} # This will hold all the scores and words (words= keys; points = values)
-
 
   def self.scoring(word)
     if word.class != String || word.length > 7
@@ -28,29 +26,69 @@ class Scrabble::Scoring
 
 
   def self.highest_score_from(array_of_words)
-    # hash_of_scores = {} #not sure if this needs to be a class variable or not
-
-    #This makes a hash of the words => scores
-    array_of_words.each do |word|
-      @@hash_of_scores["#{word}"] = "#{scoring(word)}".to_i
-    end
-
     #This finds the word that has the highest score
     max = 0
     best_word = ""
-    @@hash_of_scores.each do |wrd,sco|
-      max = sco if sco > max
-      best_word = wrd if sco == max
+    array_of_words.each do |wrd|
+      if scoring(wrd) > max
+        max = scoring(wrd)
+        best_word = wrd
+      elsif scoring(wrd) == max #same points; winner should be shorter word
+        if wrd.length < best_word.length
+          best_word = wrd
+        end
+      end
     end
-
     return best_word.upcase
   end
 
+end
+
+class Scrabble::Player
+  attr_accessor :name
+  def initialize(name)
+    @array_of_words = []
+    @name = name
+    @won = false
+    @player_total_score = 0
+  end
+
+  def play(word) #Adds the input word to the plays Array
+    return false if @won == true #Returns false if player has already won
+    @array_of_words << word
+    return Scrabble::Scoring.scoring(word)# Returns the score of the word
+  end
+
+  def plays #Returns an Array of the words played by the player
+    return @array_of_words
+  end
+
+  def total_score #Returns the sum of scores of played words
+    words = self.plays
+    words.each do |word|
+      a = Scrabble::Scoring.scoring(word)
+      @player_total_score += a
+    end
+  end
+
+  def won?
+    @won = true if @player_total_score > 100
+    return @won
+  end
+
+  def highest_scoring_word #Returns the highest scoring played word
+    return Scrabble::Scoring.highest_score_from(@array_of_words)
+  end
+
+  def highest_word_score #Returns the highest_scoring_word score
+    return Scrabble::Scoring.scoring(self.highest_scoring_word)
+  end
 
 end
 
 
-
-# puts Scrabble::Scoring.scoring("poop")
-# the_array = ["qui","qi","jazzman"]
-# ap Scrabble::Scoring.highest_score_from(the_array)
+bob = Scrabble::Player.new("Bob")
+ap bob.name
+ap bob.play("get")
+ap bob.play("jazzman")
+ap bob.plays
