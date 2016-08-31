9ad9a7758b2a54ebba33d3a7a3222806fb746fb1
Miriam Cortes <mcortes.umiami@gmail.com>
2016-08-30 17:59:22 -0700

    Added basic method for finding highest scoring word from an array

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,14 +1,15 @@
-
 require_relative '../Scrabble'
+require 'awesome_print'
 
 class Scrabble::Scoring
+  @@hash_of_scores = {} # This will hold all the scores and words (words= keys; points = values)
+
 
   def self.scoring(word)
-    if word.class != String
-      raise ArgumentError #unless word.is_a?(String)
+    if word.class != String || word.length > 7
+      raise ArgumentError
     end
     word_array = word.upcase.split(//)
-
     points = 0
     i = 0
     word_array.each do
@@ -25,8 +26,31 @@ class Scrabble::Scoring
     return points
   end
 
+
   def self.highest_score_from(array_of_words)
+    # hash_of_scores = {} #not sure if this needs to be a class variable or not
+
+    #This makes a hash of the words => scores
+    array_of_words.each do |word|
+      @@hash_of_scores["#{word}"] = "#{scoring(word)}".to_i
+    end
+
+    #This finds the word that has the highest score
+    max = 0
+    best_word = ""
+    @@hash_of_scores.each do |wrd,sco|
+      max = sco if sco > max
+      best_word = wrd if sco == max
+    end
+
+    return best_word.upcase
   end
+
+
 end
 
-# Scrabble::Scoring.scoring("QI")
+
+
+# puts Scrabble::Scoring.scoring("poop")
+# the_array = ["qui","qi","jazzman"]
+# ap Scrabble::Scoring.highest_score_from(the_array)
