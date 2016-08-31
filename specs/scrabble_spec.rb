0197e320047b347533cb3913524561664030f030
Miriam Cortes <mcortes.umiami@gmail.com>
2016-08-30 17:59:56 -0700

    added test for highest_score_from method

--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -3,31 +3,27 @@ require_relative '../lib/scrabble.rb'
 
 describe 'Testing Score' do
 
-  it 'QI => 10 + 1 = 11 points' do
+  it 'Correct points given' do
     expect( Scrabble::Scoring.scoring('QI')).must_equal(11)
-  end
-
-  it 'JAZZMAN = 84 points' do
     expect( Scrabble::Scoring.scoring('JAZZMAN')).must_equal(84)
-  end
-
-  it 'jazzman = 84 points' do
     expect( Scrabble::Scoring.scoring('jazzman')).must_equal(84)
   end
 
-  it 'Must raise an ArgumentError if given a non-string' do
+  it 'Must raise an ArgumentError' do
+    # if given a non-string:
     expect (proc {Scrabble::Scoring.scoring(4)}).must_raise ArgumentError
-  end
-
-  it 'Raises an ArgumentError if given a combination of String & Fixnum' do
+    # if given a combination of String & Fixnum
     expect (proc {Scrabble::Scoring.scoring('4J')}).must_raise ArgumentError
+    #if a word longer than 8 characters is entered' do
+    expect (proc  {Scrabble::Scoring.scoring('elizabethtown')}).must_raise ArgumentError
   end
 
+  it 'The array ["qi", "jazzman"] should return jazzman' do
+    expect (Scrabble::Scoring.highest_score_from(["qi","jazzman"])).must_equal("JAZZMAN")
+  end
 
-
-
-
-
-
+  # it 'The array ["qi", "jazzman"] should return jazzman' do
+  #   expect (Scrabble::Scoring.highest_score_from(["qi","jazzman"])).mmust_equal("JAZZMAN")
+  # end
 
 end
